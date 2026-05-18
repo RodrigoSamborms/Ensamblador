9d48e3d411d2b042fbd00d0af102106773161ce1
RodrigoSamborms <your-correct-email@example.com>
2026-05-17 22:36:16 -0600

    p6 primer parseo listo

--- Emu8086/TAREAS/Practica06/Practica06_V1.asm
+++ Emu8086/TAREAS/Practica06/Practica06_V1.asm
@@ -12,6 +12,14 @@ buffer db max_caracteres, 0, max_caracteres dup(' ')
 cadena db 60 dup(0)
 tam_cadena db 0 ;para saber cuantos elementos hay en la cadena
 cadena_binario db 60 dup(0) ;contiene los digitos en binario de la cadena original
+cadena_resultado1 db 60 dup(0) ;resultado del primer parse
+
+;Variables
+operador1 db 0 ;contenedor del primer operando
+operacion db 0 ;contenedor de la operacion a realizar
+operador2 db 0 ;contenedor del segundo operando
+resultado db 0 ;resultado de la operacion
+contador db 1 ;cuenta los caracteres
 ; Funciones de la librería emu8086.inc
 DEFINE_PRINT_STRING
 DEFINE_GET_STRING
@@ -52,20 +60,20 @@ MOV SI, offset buffer + 2;indice fuente direccion
 LEA DI, cadena  ;indice destino direccion
 MOV BL, 0
 inicio_while:
-MOV AL, [SI]        ;Tomamos el dato de la casilla fuente
-CMP AL, '$'         ;Si es el centinela terminamos
-JE fin_while
-MOV [DI], AL        ;Lo copiamos a la casilla destino
-INC SI              ;Movemos el apuntador a la siguiente direccion
-INC DI
-INC BL              ;Aumentamos la cuenta sólo si copiamos un carácter
-JMP inicio_while
+    MOV AL, [SI]        ;Tomamos el dato de la casilla fuente
+    CMP AL, '$'         ;Si es el centinela terminamos
+    JE fin_while
+    MOV [DI], AL        ;Lo copiamos a la casilla destino
+    INC SI              ;Movemos el apuntador a la siguiente direccion
+    INC DI
+    INC BL              ;Aumentamos la cuenta sólo si copiamos un carácter
+    JMP inicio_while
 fin_while: ;salimos del bucle while
-MOV [DI], '$'         ;terminador para INTs
-MOV [DI+1], 0         ;terminador para PRINT_STRING
-MOV [tam_cadena], BL ;guardamos el numero de elementos en la cadena
-
-;MOV BL, 0
+    MOV [DI], '$'         ;terminador para INTs
+    MOV [DI+1], 0         ;terminador para PRINT_STRING
+    MOV [tam_cadena], BL ;guardamos el numero de elementos en la cadena
+    
+    ;MOV BL, 0
 
 PRINTN '';salto de linea
 LEA    SI, cadena       ;cargamos el mensaje a enviar
@@ -93,21 +101,21 @@ LEA SI, cadena ;cadena de origen
 LEA DI, cadena_binario ;cadena destino
 MOV BL, 0   ;preparamos el contador de caracteres
 ASCII_Binario:
-MOV AL, [SI] ;leemos el primer dato
-;inicia_conversion ASCCII->Binario
-CMP AL, 30h    ;verificamos el rango menor
-JB no_digito   ;si el caracter esta dentro
-CMP AL, 39h    ;verificamos el rango mayor
-JA no_digito   ;en caso de no ser digito
-SUB AL, 30h    ;restamos 30h para convertir
-JMP termina_conversion
+    MOV AL, [SI] ;leemos el primer dato
+    ;inicia_conversion ASCCII->Binario
+    CMP AL, 30h    ;verificamos el rango menor
+    JB no_digito   ;si el caracter esta dentro
+    CMP AL, 39h    ;verificamos el rango mayor
+    JA no_digito   ;en caso de no ser digito
+    SUB AL, 30h    ;restamos 30h para convertir
+    JMP termina_conversion
 no_digito:
-;no hacemos nada al dato se envia un mensaje de
-;error en caso de ser requerido dejado a futuro
-;para revision si es cadena valida
+    ;no hacemos nada al dato se envia un mensaje de
+    ;error en caso de ser requerido dejado a futuro
+    ;para revision si es cadena valida
 termina_conversion:
-;se ha terminado la conversion
-;independientemente si se convirtio o no
+    ;se ha terminado la conversion
+    ;independientemente si se convirtio o no
 MOV [DI], AL ;Movemos el dato convertido a la cadena destino
 INC SI   ;Nos movemos al siguiente caracter
 INC DI   ;en las cadenas fuente SI destino DI
@@ -127,6 +135,114 @@ XOR AX, AX
 MOV AL, [tam_cadena]
 call PRINT_NUM_UNS
 PRINT ''
+
+
+;detemos para debug paso a paso
+PRINT "cambie a ejecucion paso a paso para DEBUG"
+mov ah, 0
+int 16h
+
+
+
+;NOTA: el siguiente codigo espera que la cadena sea
+;valida y el minimo de cadena es de tres elementos
+;mas el caracter '$', por ejemplo:
+; op1,operacion,op2,$
+
+;Ahora vamos a procesar la cadena
+LEA SI, cadena_binario ;cadena de origen
+LEA DI, cadena_resultado1 ;cadena destino
+MOV BL, contador   ;preparamos el contador de caracteres
+MOV CL, [SI];caracter de fin de cadena '$'
+MOV CH, 0   ;parte alta 0
+XOR AX, AX ;Resetamos AX a 0 AH=0 AL=0
+XOR BX, BX ;Resetamos BX a 0 BH=0 BL=0
+recorrer_cadena:
+;obtenemos los datos operando1 operacion operando2
+    MOV AL, [SI] ;tomamos el primer operador
+    MOV operador1, AL ;y lo guardamos 
+    INC SI            ;nos movemos al caracter de  
+    MOV AL,[SI]       ;de la operacion
+    MOV operacion, AL ;y lo guardamos
+    INC SI            ;segundo operador
+    MOV AL, [SI]      ;lo leemos
+    MOV operador2, AL ;y lo guardamos                
+;procesamos los operandos acorde al operador
+    MOV AL, operacion
+    CMP AL, '/' ;la operacion es division?
+    JNE es_multiplicacion
+    JMP aplicar_division
+es_multiplicacion:
+    CMP AL, '*'   ;la operacion es multiplicacion?
+    JNE no_parsear
+    JMP aplicar_multiplicacion
+aplicar_division:
+;division de bytes cociente en AL residuo AH 
+    MOV AL, operador1 ;dividendo
+    MOV BL, operador2 ;divisor
+    DIV BL  ;aplicamos division
+    MOV resultado, AL ;recuperamos el cociente
+    JMP guardar_en_arreglo 
+aplicar_multiplicacion:
+;multiplicacin de bytes, nota resultado no es mayor a 255 un byte
+;debido a que es un digito solamente
+    MOV AL, operador1 ;multiplicando
+    MOV BL, operador2 ;multiplicador
+    MUL BL  ;aplicamos multiplicacion
+    MOV resultado, AL ;recuperamos el resultado
+    JMP guardar_en_arreglo ;este salto parece innecesario se deja para mantener logica
+guardar_en_arreglo:
+    MOV AL, resultado ;cargamos el resultado
+    MOV [DI], AL;guardamos en la cadena_resultado1
+    MOV [SI], AL;
+    ;solo nos movemos a la siguiente casilla cuando no halla parseo    
+    MOV [DI+1], '$' ;Agregamos el fin de linea
+    MOV [DI+2], 0   ;terminador de linea
+    MOV BL, contador
+    ADD BL, 0      ;se agregan 0 caracteres
+    MOV contador, BL
+    XOR BL, BL     ;
+    JMP termina_parseo
+no_parsear:
+    ;guardamos los valores en la cadena sin reducir
+    MOV AL, operador1
+    MOV [DI], AL
+    INC DI
+    MOV AL, operacion
+    MOV [DI], AL
+    INC DI
+    MOV AL, operador2
+    MOV [DI], AL
+    ;DI apunta al siguiente carcter en cadena_resultado1
+    MOV [DI+1], '$' ;agregamos el fin de linea
+    MOV [DI+2], 0   ;terminador de linea
+    MOV BL, contador
+    ADD BL, 2      ;se agregan tres caracteres
+    MOV contador, BL
+    XOR BL, BL     ;
+termina_parseo:    
+    ;SI esta posicionado dos espacios adelante
+    ;verificamos si el siguiente elemento es un '$'
+    ;que indica el final de la cadena
+    MOV CL, [SI+1]                                       
+    CMP CL, '$' ;Fin de la cadena?
+    JNE recorrer_cadena
+;MOV [DI], '$'         ;terminador para INTs
+;MOV [DI+1], 0         ;terminador para PRINT_STRING
+MOV BL, contador
+MOV [tam_cadena], BL ;guardamos el numero de elementos en la cadena    
+    
+;veerificamos el resultado
+PRINTN '';salto de linea
+LEA    SI, cadena_resultado1 ;cargamos el mensaje a enviar
+CALL   PRINT_STRING   ;imprimimos el mensaje
+PRINTN ''
+PRINT "tamio de la cadena: "
+XOR AX, AX
+MOV AL, [tam_cadena]
+call PRINT_NUM_UNS
+PRINT ''
+
 
 
 Fin_programa:
